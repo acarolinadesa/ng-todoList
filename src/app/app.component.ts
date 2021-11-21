97b4afb6f51386105b160f3673361564eedbd5b0
acarolren <anacarolinareisdesa@gmail.com>
2021-11-21 16:02:09 -0300

    Add Create and Read

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-todoList';
+  title = 'Ng TodoList';
+  tasks : Array<String> = [
+    'item1',
+    'item2'
+  ]
+  task : String = ''
+
+  cadastrar () {
+    this.tasks.push(this.task)
+    this.task = ''
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { FormTodoComponent } from './form-todo/form-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormTodoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
